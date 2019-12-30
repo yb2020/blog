36237e0f241024340ee43bea7be8ca3eccaea169
yb2020 <20207745@qq.com>
2019-12-31 04:16:18 +0800

    修复局域网不能访问的问题

--- config.js
+++ config.js
@@ -1,21 +1,20 @@
-const IS_DEV = process.env.NODE_ENV !== 'production';
+const IS_DEV = process.env.NODE_ENV !== "production";
 
-const API_ROOT = IS_DEV ? 'http://localhost:9999/' : 'https://api.jkchao.cn/api/';
+const API_ROOT = IS_DEV ? "http://localhost:9999/" : "http://localhost:9999/";
 
-const STATIC_PATH = 'http://static.yi.wuhuhai.com';
+const STATIC_PATH = "http://static.yi.wuhuhai.com";
 
-const CDN_PATH = 'https://cdn.jkchao.cn/nuxt';
+const CDN_PATH = "https://cdn.jkchao.cn/nuxt";
 
-const SERVER_HOST = '0.0.0.0';
+const SERVER_HOST = "0.0.0.0";
 const SERVER_PORT = 3000;
-
 
 const QINIU_OPTIONS = {
   // qiniu accessKey secretKey
   accessKey: process.env.ACCESS_KEY,
   secretKey: process.env.SECRET_KEY,
-  bucket: 'blog-cdn',
-  domain: 'http://ply4cszel.bkt.clouddn.com'
+  bucket: "blog-cdn",
+  domain: "http://ply4cszel.bkt.clouddn.com"
 };
 
 module.exports = {
