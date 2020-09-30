54993c75bc2199d4d3a5ea22731caa05e054f26d
yb2020 <20207745@qq.com>
2020-09-30 11:54:17 +0800

    切换微服务

--- api/service.js
+++ api/service.js
@@ -1,157 +1,174 @@
-import serviceApi from './axios'
-const serviceId = '/MICROSERVICE-APP-BLOG/blog'
-
-
+import serviceApi from "./axios";
+const serviceId = "/microService-app-blog/blog";
 
 // 获取用户信息
-export async function getAuth () {
+export async function getAuth() {
   return serviceApi({
-    url: serviceId + '/author/getAuthor',
-    method: 'post',
+    url: serviceId + "/author/getAuthor",
+    method: "post",
     params: null
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 获取网站配置项
-export function getOpt () {
+export function getOpt() {
   return serviceApi({
-    url: serviceId + '/site/getSite',
-    method: 'post',
+    url: serviceId + "/site/getSite",
+    method: "post",
     params: null
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 英雄版列表
-export function getHero (params) {
-
-  return ax.get(serviceId + '/hero', { params })
-          .then(res => res.data)
+export function getHero(params) {
+  return ax.get(serviceId + "/hero", { params }).then(res => res.data);
 }
 
 // 增加英雄榜
-export function postHero (data) {
-  return ax.post('/hero', {...data})
-          .then(res => res.data)
+export function postHero(data) {
+  return ax.post("/hero", { ...data }).then(res => res.data);
 }
 
 // 标签列表
-export function getTag (params) {
+export function getTag(params) {
   return serviceApi({
-    url: serviceId + '/tag/all',
-    method: 'post',
+    url: serviceId + "/tag/all",
+    method: "post",
     params: params
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 根据tag获取文章列表
-export function getArts (params) {
+export function getArts(params) {
   return serviceApi({
-    url: serviceId + '/article/list' ,
-    method: 'post',
+    url: serviceId + "/article/list",
+    method: "post",
     data: params
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 根据随机获取获取文章列表
-export function getRandomArts (params) {
+export function getRandomArts(params) {
   return serviceApi({
-    url: serviceId + '/article/randomList' ,
-    method: 'post',
+    url: serviceId + "/article/randomList",
+    method: "post",
     data: params
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 文章归档
-export function getAllArts () {
+export function getAllArts() {
   return serviceApi({
-    url: serviceId + '/article/all',
-    method: 'post',
+    url: serviceId + "/article/all",
+    method: "post",
     params: null
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 获取单个文章
-export function getArt (data) {
+export function getArt(data) {
   return serviceApi({
     url: serviceId + `/article/getByUrl/${data.id}`,
-    method: 'get',
+    method: "get",
     params: null
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 文章点赞
-export function likeArt (data) {
+export function likeArt(data) {
   return serviceApi({
     url: serviceId + `/article/likeByUrl/${data.id}`,
-    method: 'get',
+    method: "get",
     params: null
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 获取评价
-export function getComments (data) {
+export function getComments(data) {
   return serviceApi({
     url: serviceId + `/comment/getComments`,
-    method: 'post',
+    method: "post",
     data: data
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 提交评价
-export function postComment (data) {
+export function postComment(data) {
   return serviceApi({
     url: serviceId + `/comment/postComment`,
-    method: 'post',
+    method: "post",
     data: data
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 评论点赞
-export function likeComment (data) {
+export function likeComment(data) {
   return serviceApi({
     url: serviceId + `/comment/likeComment/${data.id}`,
-    method: 'get'
-  }).then(res => res.data).catch(err => console.error(err))
+    method: "get"
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 音乐列表
-export function getMusicLst (data) {
-  return ax.get('/music/list/962562705')
-          .then(res => res.data)
+export function getMusicLst(data) {
+  return ax.get("/music/list/962562705").then(res => res.data);
 }
 
 // 音乐详情
-export function getMusicDetail (para) {
-  return ax.get(`/music/song/${para.song_id}`)
-          .then(res => res.data)
+export function getMusicDetail(para) {
+  return ax.get(`/music/song/${para.song_id}`).then(res => res.data);
 }
 
 // 音乐地址
-export function getMusicUrl (para) {
-  return ax.get(`/music/url/${para.id}`)
-          .then(res => res.data)
+export function getMusicUrl(para) {
+  return ax.get(`/music/url/${para.id}`).then(res => res.data);
 }
 
 // 获取书本
-export function getBook (params) {
-  return ax.get(`/book`, { params })
-          .then(res => res.data)
+export function getBook(params) {
+  return ax.get(`/book`, { params }).then(res => res.data);
 }
 
 // 获取友链
-export function getLink (params) {
+export function getLink(params) {
   return serviceApi({
-    url: serviceId + '/link/getLink',
-    method: 'post',
+    url: serviceId + "/link/getLink",
+    method: "post",
     params: params
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
 
 // 获取导航
-export function getNav (params) {
+export function getNav(params) {
   return serviceApi({
-    url: serviceId + '/nav/getNav/' + params.position,
-    method: 'post',
+    url: serviceId + "/nav/getNav/" + params.position,
+    method: "post",
     params: null
-  }).then(res => res.data).catch(err => console.error(err))
+  })
+    .then(res => res.data)
+    .catch(err => console.error(err));
 }
